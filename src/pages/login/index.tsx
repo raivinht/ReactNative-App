59de28e46e03f456f059db214a97ef324154eb2f
Arthur Nogaroto <a.h.nogaroto@gmail.com>
2025-08-18 14:47:36 -0300

    first commit

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { style } from "./styles"
+
+export default function Login() {
+    return (
+        <View style={style.container}>
+            <View style={style.boxTop}>
+                <Text>Top</Text>
+            </View>
+            <View style={style.boxMid}>
+                <Text>Mid</Text>
+            </View>
+            <View style={style.boxBottom}>
+                <Text>Bottom</Text>
+            </View>
+        </View>
+    )
+}
