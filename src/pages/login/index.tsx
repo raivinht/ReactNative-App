9975caa3937f26db2bb618b2e7973fec06f55036
Arthur Nogaroto <a.h.nogaroto@gmail.com>
2025-08-18 15:38:45 -0300

    Text, Image & TextInput added, and @types

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, TextInput } from "react-native";
 import { style } from "./styles"
+import Logo from '../../assets/logo.png';
 
 export default function Login() {
     return (
         <View style={style.container}>
             <View style={style.boxTop}>
                 <Text>Top</Text>
+                <Image source={Logo} />
+                <Text>Bem vindo de volta!</Text>
             </View>
             <View style={style.boxMid}>
                 <Text>Mid</Text>
+                <Text>Endereço de E-mail</Text>
+                <TextInput />
+                <Text>Senha</Text>
+                <TextInput />
             </View>
             <View style={style.boxBottom}>
                 <Text>Bottom</Text>
