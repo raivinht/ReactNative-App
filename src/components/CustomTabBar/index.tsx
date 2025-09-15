ec1354afd45ed01017c03cdde30b64423bcc8ce6
Arthur Nogaroto <a.h.nogaroto@gmail.com>
2025-09-15 11:45:54 -0300

    Creating buttons for each component in the second screen, then applying icons fot better performance and centralizing them.

--- src/components/CustomTabBar/index.tsx
+++ src/components/CustomTabBar/index.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
+import { AntDesign, FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons'
 import { style } from "./styles";
 
 export default ({state, navigation}: any) => {
     return (
-        <View>
-            <TouchableOpacity>
-                <Text>Esquerda</Text>
+        <View style={style.tabArea}>
+            <TouchableOpacity style={style.tabItem}>
+                <AntDesign
+                    name="bars"
+                    style={{ fontSize: 32 }}
+                />
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>Centro</Text>
+            <TouchableOpacity style={style.tabItemButton}>
+                <View>
+                    <Entypo
+                        name="plus"
+                        size={40}
+                    />
+                </View>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>Direita</Text>
+            <TouchableOpacity style={style.tabItem}>
+                <FontAwesome
+                    name="user"
+                    style={{ fontSize: 32 }}
+                />
             </TouchableOpacity>
         </View>
     )
--- src/components/CustomTabBar/styles.ts
+++ src/components/CustomTabBar/styles.ts
@@ -6,5 +6,20 @@ export const style = StyleSheet.create({
         flexDirection: 'row',
         height: 80,
         justifyContent: 'space-around',
+    },
+    tabItem: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tabItemButton: {
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 9999,
+        top: -30,
+        backgroundColor: themes.colors.primary,
     }
 })
