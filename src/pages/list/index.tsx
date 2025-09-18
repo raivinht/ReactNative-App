7ba0eb32d32defcd92945f5b81fe446a01859cc5
Arthur Nogaroto <a.h.nogaroto@gmail.com>
2025-09-18 08:07:03 -0300

    Creating the header of the List, applying styles and text for the header.

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { style } from "./styles";
+import { Input } from "../../components/input";
+import { MaterialIcons } from '@expo/vector-icons' 
 
 export default function List() {
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Lista</Text>
+        <View style={style.container}>
+            <View style={style.header}>
+                <Text style={style.greeting}>Bom dia,
+                    <Text style={{ fontWeight: 'bold' }}>Arthur Nogaroto</Text>
+                </Text>
+                <View style={style.boxInput}>
+                    <Input 
+                        IconLeft={MaterialIcons}
+                        IconLeftName="search"
+                    />
+                </View>
+            </View>
         </View>
     )
 }
--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -2,5 +2,23 @@ import { StyleSheet, Dimensions } from "react-native";
 import { themes } from "../../global/themes";
 
 export const style = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    header: {
+        width: '100%',
+        height: Dimensions.get('window').height/6,
+        backgroundColor: themes.colors.primary,
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+    },
+    greeting: {
+        fontSize: 20,
+        color: '#FFF',
+        marginTop: 20,
+    },
+        boxInput: {
+        width: '80%',
+    },
 })
